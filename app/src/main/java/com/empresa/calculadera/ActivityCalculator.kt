e87425c0ec0729e7c805a1e7a07503ee53e3c0ed
Filipe Tavares <filipertavares9@gmail.com>
2023-01-10 18:07:09 -0300

    calculadora

---
+++ app/src/main/java/com/empresa/calculadera/ActivityCalculator.kt
@@ -0,0 +1,77 @@
+package com.empresa.calculadera
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+
+class ActivityCalculator : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.calculator_layout)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        val numbersId = listOf(
+            R.id.zero,
+            R.id.one,
+            R.id.two,
+            R.id.three,
+            R.id.four,
+            R.id.five,
+            R.id.six,
+            R.id.seven,
+            R.id.eight,
+            R.id.nine,
+            R.id.dot
+        )
+
+        val operationsId = listOf(
+            R.id.multiply,
+            R.id.division,
+            R.id.plus,
+            R.id.decrease
+        )
+
+        val result = R.id.result
+        val erease = R.id.erease
+
+        findViewById<Button>(result).setOnClickListener {
+            viewModel.result()
+        }
+
+        findViewById<Button>(erease).setOnClickListener {
+            viewModel.erease()
+        }
+
+        numbersId.forEach {
+            bindNumbersClick(findViewById(it))
+        }
+
+        operationsId.forEach {
+            bindOperationsClick(findViewById(it))
+        }
+
+        viewModel.resultView.observe(this) {
+            findViewById<TextView>(R.id.result_view).text = it
+        }
+
+    }
+
+    private fun bindNumbersClick(componnet: View) {
+        componnet.setOnClickListener {
+            viewModel.updateDisplay((it as Button).text.toString())
+        }
+    }
+
+    private fun bindOperationsClick(componnet: View) {
+        componnet.setOnClickListener {
+            viewModel.operation((it as Button).text.toString())
+        }
+    }
+
+}
---
+++ app/src/main/java/com/empresa/calculadera/MainViewModel.kt
@@ -0,0 +1,49 @@
+package com.empresa.calculadera
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    private val result = MutableLiveData("")
+    val resultView: LiveData<String> = result
+    private var calcValue: Double = 0.0
+    private var operation: String = ""
+
+
+    fun updateDisplay(numbers: String) {
+        result.value += numbers
+    }
+
+    fun operation(operation: String) {
+        if (calcValue == 0.0) {
+            calcValue = (result.value)?.toDouble()!!
+            result.value = ""
+        }
+        this.operation = operation
+    }
+
+    fun result() {
+
+        var value: Double? = ((result.value)?.toDouble())
+
+        when {
+            operation == "*" -> result.value = (calcValue * value!!).toString()
+            operation == "/" -> result.value = (calcValue / value!!).toString()
+            operation == "-" -> result.value = (calcValue - value!!).toString()
+            operation == "+" -> result.value = (calcValue + value!!).toString()
+        }
+
+        calcValue = 0.0
+
+    }
+
+    fun erease() {
+        result.value = ""
+        calcValue = 0.0
+        operation = ""
+    }
+
+
+}
